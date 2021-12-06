5e6a8fa70222f2eae806eb931748661c29f70561
unknown <jeferson_cambui@hotmail.com>
2021-12-06 19:27:52 -0300

    Minha primeira e segunda aplicação Spring

---
+++ Modulo 2/Spring/Minha segunda aplicação Spring/src/main/java/br/org/generation/helloword/controller/HelloController.java
@@ -0,0 +1,18 @@
+package br.org.generation.helloword.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Atividade 2 - Minha segunda aplicação Spring
+
+@RestController
+@RequestMapping("/hello") 
+public class HelloController {
+    
+	@GetMapping
+    public String helloWord() {
+
+        return "Praticar Spring, entender melhor sobre metodos, revisar atividades já feita em Java";
+    }
+}
