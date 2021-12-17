1c5b69e9df8eee6768af81adfddab9e494ce4907
unknown <jeferson_cambui@hotmail.com>
2021-12-17 19:06:24 -0300

    Atualizando a atividade

--- Modulo 2/Spring/Deploy BlogPessoal/src/main/java/BlogPessoal/model/Usuario.java
+++ Modulo 2/Spring/Deploy BlogPessoal/src/main/java/BlogPessoal/model/Usuario.java
@@ -9,10 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "tb_usuarios")
@@ -25,8 +28,11 @@ public class Usuario {
 	@NotNull(message = "O atributo nome é obrigatório")
 	private String nome;
 	
-	@NotNull(message = "O atributo usuário é obrigatório")
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
+
 	
 	@NotNull(message = "O atributo senha é obrigatório")
 	@Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres")
--- Modulo 2/Spring/Deploy BlogPessoal/src/main/java/BlogPessoal/security/BasicSecurityConfig.java
+++ Modulo 2/Spring/Deploy BlogPessoal/src/main/java/BlogPessoal/security/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package BlogPessoal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -19,29 +21,34 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		
+		 auth.userDetailsService(userDetailsService);
 
-		auth.inMemoryAuthentication()
+		 auth.inMemoryAuthentication()
 			.withUser("root")
 			.password(passwordEncoder().encode("root"))
 			.authorities("ROLE_USER");
-			
-	}
 
+	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Override
+	
+	 @Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		http.authorizeRequests()
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
+			.antMatchers(HttpMethod.OPTIONS).permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and().cors()
 			.and().csrf().disable();
+			
 	}
 }
