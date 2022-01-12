e712fdbb5caa06b325add973f6bc8d96a851df1d
unknown <jeferson_cambui@hotmail.com>
2022-01-12 20:34:46 -0300

    Atividade Entregavel 3

---
+++ Modulo 3/Fundamentos Web/assets/js/script.js
@@ -0,0 +1,44 @@
+/*
+Case Sensitive = reconhece letras maiusculas e minusculas
+
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe: getElementByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome(){
+
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3){
+        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+    }
+
+}
+
+function validaEmail(){
+
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.com') == -1) {
+        txtEmail.innerHTML = 'E-mail Inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail Válido'
+        txtEmail.style.color = 'green'
+    }
+
+}
