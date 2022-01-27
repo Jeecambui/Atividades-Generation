f994ceac35e9db0d478076398586ac4479bf9654
Jeferson <47721080+Jeecambui@users.noreply.github.com>
2022-01-27 16:15:12 -0300

    Update Service.ts

--- Modulo 3/React/blog-pessoal/src/services/Service.ts
+++ Modulo 3/React/blog-pessoal/src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://projeto-blogpessoal.herokuapp.com/'
+    baseURL: 'https://projeto-blogpessoal.herokuapp.com'
 })
 
 export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
